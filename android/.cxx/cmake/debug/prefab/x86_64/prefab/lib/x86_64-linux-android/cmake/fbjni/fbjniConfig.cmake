53d94d8a839c7a9b73ff4a0247342ffcb75694d5
Janic Duplessis <janicduplessis@gmail.com>
2022-03-08 13:11:52 -0500

    gitpkg

---
+++ android/.cxx/cmake/debug/prefab/x86_64/prefab/lib/x86_64-linux-android/cmake/fbjni/fbjniConfig.cmake
@@ -0,0 +1,7 @@
+add_library(fbjni::fbjni SHARED IMPORTED)
+set_target_properties(fbjni::fbjni PROPERTIES
+    IMPORTED_LOCATION "/Users/janicduplessis/.gradle/caches/transforms-3/23bc21dceb362f27420b5a72405e1d07/transformed/jetified-fbjni-0.2.2/prefab/modules/fbjni/libs/android.x86_64/libfbjni.so"
+    INTERFACE_INCLUDE_DIRECTORIES "/Users/janicduplessis/.gradle/caches/transforms-3/23bc21dceb362f27420b5a72405e1d07/transformed/jetified-fbjni-0.2.2/prefab/modules/fbjni/include"
+    INTERFACE_LINK_LIBRARIES ""
+)
+
